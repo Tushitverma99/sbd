8a39d1fea7e413b818f76b8a9cf61086af4eb7ac
tushit <vermatushit@gmail.com>
2024-11-03 21:19:56 +0530

    Scala Concepts

--- src/main/scala/practice_set.scala
+++ src/main/scala/practice_set.scala
@@ -12,65 +12,67 @@ object practice_set{
     import spark.implicits._
 
 
-//    val employees = List(
-//      ("karthik", "2024-11-01"),
-//      ("neha", "2024-10-20"),
-//      ("priya", "2024-10-28"),
-//      ("mohan", "2024-11-02"),
-//      ("ajay", "2024-09-15"),
-//      ("vijay", "2024-10-30"),
-//      ("veer", "2024-10-25"),
-//      ("aatish", "2024-10-10"),
-//      ("animesh", "2024-10-15"),
-//      ("nishad", "2024-11-01"),
-//      ("varun", "2024-10-05"),
-//      ("aadil", "2024-09-30")
-//    ).toDF("name", "last_checkin")
-//
-//    val resultDF = employees
-//      .withColumn("last_checkin", to_date(col("last_checkin"), "yyyy-MM-dd"))
-//      .withColumn("name", initcap(col("name")))
-//      .withColumn("status", when(datediff(current_date(), col("last_checkin")) <= 7, "Active" ).otherwise("Inactive"))
-//
-//    resultDF.show()
-//
-//    employees.createOrReplaceTempView("employees_sql")
-//    val sql_result = spark.sql(
-//      """
-//        |select initcap(name) as name,
-//        |case when datediff(current_date() - to_date(last_checkin, 'yyyy-MM-dd')) <= 7 then 'Active'
-//        |else 'Inactive'
-//        |end as status
-//        |from employee_sql
-//        |""".stripMargin)
-//    sql_result.show()
-//
-//
-//    ///////////question-2/////////////
-//
-//
-//    val sales = List(
-//      ("karthik", 60000),
-//      ("neha", 48000),
-//      ("priya", 30000),
-//      ("mohan", 24000),
-//      ("ajay", 52000),
-//      ("vijay", 45000),
-//      ("veer", 70000),
-//      ("aatish", 23000),
-//      ("animesh", 15000),
-//      ("nishad", 8000),
-//      ("varun", 29000),
-//      ("aadil", 32000)
-//
-//    ).toDF("name", "total_sales")
-//
-//    val salesDF = sales
-//      .withColumn("name", initcap(col("name")))
-//      .withColumn("performance_status", when(col("total_sales") > 50000, "Excellent")
-//        .when(col("total_sales")>25000 && col("total_sales")<50000, "Good").otherwise("Needs Improvement"))
-//    val aggSalesdf = salesDF.groupBy(col("performance_status")).agg(sum(col("total_sales"))).alias("agg_sales")
-//    aggSalesdf.show()
+    val employees = List(
+      ("karthik", "2024-11-01"),
+      ("neha", "2024-10-20"),
+      ("priya", "2024-10-28"),
+      ("mohan", "2024-11-02"),
+      ("ajay", "2024-09-15"),
+      ("vijay", "2024-10-30"),
+      ("veer", "2024-10-25"),
+      ("aatish", "2024-10-10"),
+      ("animesh", "2024-10-15"),
+      ("nishad", "2024-11-01"),
+      ("varun", "2024-10-05"),
+      ("aadil", "2024-09-30")
+    ).toDF("name", "last_checkin")
+
+    val resultDF = employees
+      .withColumn("last_checkin", to_date(col("last_checkin"), "yyyy-MM-dd"))
+      .withColumn("name", initcap(col("name")))
+      .withColumn("status", when(datediff(current_date(), col("last_checkin")) <= 7, "Active" ).otherwise("Inactive"))
+
+    resultDF.show()
+
+    employees.createOrReplaceTempView("employees_sql")
+    val sql_result = spark.sql(
+      """
+        |select initcap(name) as name,
+        |case when datediff(current_date() - to_date(last_checkin, 'yyyy-MM-dd')) <= 7 then 'Active'
+        |else 'Inactive'
+        |end as status
+        |from employee_sql
+        |""".stripMargin)
+    sql_result.show()
+
+
+    ///////////question-2/////////////
+
+
+    val sales = List(
+      ("karthik", 60000),
+      ("neha", 48000),
+      ("priya", 30000),
+      ("mohan", 24000),
+      ("ajay", 52000),
+      ("vijay", 45000),
+      ("veer", 70000),
+      ("aatish", 23000),
+      ("animesh", 15000),
+      ("nishad", 8000),
+      ("varun", 29000),
+      ("aadil", 32000)
+
+    ).toDF("name", "total_sales")
+
+    val salesDF = sales
+      .withColumn("name", initcap(col("name")))
+      .withColumn("performance_status", when(col("total_sales") > 50000, "Excellent")
+        .when(col("total_sales")>25000 && col("total_sales")<50000, "Good").otherwise("Needs Improvement"))
+    val aggSalesdf = salesDF.groupBy(col("performance_status")).agg(sum(col("total_sales"))).alias("agg_sales")
+    aggSalesdf.show()
+
+    /////////////question 3///////////////////////////
 
     val workload = List(
       ("karthik", "ProjectA", 120),
