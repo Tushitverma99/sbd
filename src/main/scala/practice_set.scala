6bfd878cee769c6efe592c52698340a233b71bb3
tushit <vermatushit@gmail.com>
2024-11-03 19:57:14 +0530

    Scala Concepts

---
+++ src/main/scala/practice_set.scala
@@ -0,0 +1,39 @@
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, current_date, datediff, initcap, to_date, when}
+
+object practice_set{
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("Spark")
+      .master("local[*]")
+      .getOrCreate()
+    import spark.implicits._
+
+
+    val employees = List(
+      ("karthik", "2024-11-01"),
+      ("neha", "2024-10-20"),
+      ("priya", "2024-10-28"),
+      ("mohan", "2024-11-02"),
+      ("ajay", "2024-09-15"),
+      ("vijay", "2024-10-30"),
+      ("veer", "2024-10-25"),
+      ("aatish", "2024-10-10"),
+      ("animesh", "2024-10-15"),
+      ("nishad", "2024-11-01"),
+      ("varun", "2024-10-05"),
+      ("aadil", "2024-09-30")
+    ).toDF("name", "last_checkin")
+
+    val resultDF = employees
+      .withColumn("last_checkin", to_date(col("last_checkin"), "yyyy-MM-dd"))
+      .withColumn("name", initcap(col("name")))
+      .withColumn("status", when(datediff(current_date(), col("last_checkin")) <= 7, "Active" ).otherwise("Inactive"))
+
+    resultDF.show()
+
+
+  }
+}
